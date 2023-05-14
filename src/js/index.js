42895c277d4f70f046f4360929b25b575d17eaf3
Shystra <jlsp.2019@hotmail.com>
2023-05-14 13:12:07 -0300

    just edit in css

--- src/js/index.js
+++ src/js/index.js
@@ -13,6 +13,7 @@ buttonChangeTheme.addEventListener("click", () => {
 
     const darkModeActtive = body.classList.contains("dark-mode-background");
         body.classList.toggle("dark-mode-background");
+            body.classList.toggle("dark-mode");
 
     if(darkModeActtive){
         imageButtonChangeTheme.setAttribute("src", "./imagens/sun.png");
